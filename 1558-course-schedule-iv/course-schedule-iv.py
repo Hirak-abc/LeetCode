5824755d5c5f27b8340b066214efe8f50d683c4d
Hirak Nath <hiraknath816@gmail.com>
2026-09-23 22:22:02 +0530

    Time: 3 ms (89.66%) | Memory: 12.9 MB (24.14%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+
+        g = [[] for _ in range(numCourses)]
+
+        for a, b in prerequisites:
+            g[a].append(b)
+
+        memo = {}
+
+        def dfs(u, v):
+            if (u, v) in memo:
+                return memo[(u, v)]
+
+            for x in g[u]:
+                if x == v or dfs(x, v):
+                    memo[(u, v)] = True
+                    return True
+
+            memo[(u, v)] = False
+            return False
+
+        return [dfs(u, v) for u, v in queries]
